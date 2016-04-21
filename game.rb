377c75af1cb25ed7b323f837478f8ff9aee0c529
Destroyergm <stefan_vg@hotmail.com>
2016-04-21 06:02:59 +0200

    Add switch_player function

--- game.rb
+++ game.rb
@@ -1,2 +1,19 @@
 class Game
+	attr_accessor :rows, :cols, :grid, :active_player
+
+	def start_game
+		@rows = 6
+		@cols = 7
+		@grid = Array.new(rows) {Array.new(cols)}
+		for i in 0...@rows
+			for j in 0...@cols
+				@grid[i][j] = 0
+			end
+		end
+		@active_player = 1
+	end
+
+	def switch_player
+		@active_player == 1 ? @active_player = 2 : @active_player = 1
+	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,20 @@
 require_relative "../game.rb"
 
-describe "Game" do
+describe Game do
 	let(:game) { Game.new }
 
 	describe "#start_game" do
-		it "Initializes the game start data" do
+
+		it "Initializes the rows to 6" do
 			game.start_game
 			expect(game.rows).to eql 6
+		end
+		it "Initializes the colums to 7" do
+			game.start_game
 			expect(game.cols).to eql 7
+		end
+		it "Initializes the grid to empty" do
+			game.start_game
 			expect(game.grid).to eql [
 				[0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0],
@@ -16,6 +23,29 @@ describe "Game" do
 				[0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0]
 			]
+		end
+
+		it "Sets the active player to 1" do
+			game.start_game
+			expect(game.active_player).to eql 1
+		end
+	end
+
+	describe "#switch_player" do
+		context "Last turn was player1's" do
+			it "Changes the active player to 2" do
+				game.active_player = 1
+				game.switch_player
+				expect(game.active_player).to eql 2
+			end
+		end
+
+		context "Last turn was player2's" do
+			it "Changes the active player to 1" do
+				game.active_player = 2
+				game.switch_player
+				expect(game.active_player).to eql 1
+			end
 		end
 	end
 end
